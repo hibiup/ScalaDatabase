6f36fd6d37d942c4abc306c1dc0466dcaaf1c702
326487162 <Welcome@661>
2019-01-18 14:23:25 -0500

    Update table name and column name to uppercase

--- src/test/resources/init.sql
+++ src/test/resources/init.sql
@@ -1,14 +1,24 @@
 -- Destroy
-DROP TABLE IF EXISTS users ;
+DROP TABLE IF EXISTS Users;
+DROP TABLE IF EXISTS Accounts;
 
 -- Creation
-CREATE TABLE users(
+CREATE TABLE Accounts(
   id INT AUTO_INCREMENT PRIMARY KEY,
   email VARCHAR(255) NOT NULL,
   password VARCHAR(255),
-  lasttime TIMESTAMP AS CURRENT_TIMESTAMP,
-  UNIQUE KEY email(email)
+  last_time TIMESTAMP AS CURRENT_TIMESTAMP,
+  UNIQUE KEY EMAIL(EMAIL)
+);
+
+CREATE TABLE Users(
+  id INT AUTO_INCREMENT PRIMARY KEY,
+  user_id INT NOT NULL,
+  first_name VARCHAR(255),
+  last_name varchar(255),
+  foreign key (user_id) references Users(user_id)
 );
 
 -- Initial data set
-INSERT INTO users(email, password) VALUES('sample@gmail.com','password');
+INSERT INTO ACCOUNTS(EMAIL, PASSWORD) VALUES('sample@gmail.com','password');
+INSERT INTO USERS(USER_ID, FIRST_NAME, LAST_NAME) VALUES(1, 'John','Smith');
--- src/test/scala/com/hibiup/database/Example_1_WithResource_Test.scala
+++ src/test/scala/com/hibiup/database/Example_1_WithResource_Test.scala
@@ -4,10 +4,18 @@ class Example_1_WithResource_Test extends Init {
     "withResource " should "closes connection automatically" in {
         withResource(dataSource.getConnection) {conn =>
             transactional(conn) { conn =>
-                val stmt = conn.prepareStatement("SELECT * FROM users")
+                    /** 未知的情况是所有的 table name 和字段名都需要转变成大写 */
+                val stmt = conn.prepareStatement(
+                    """SELECT *
+                      |FROM "USERS" AS "u"
+                      |JOIN "ACCOUNTS" AS "a"
+                      |ON "u"."USER_ID" = "a"."ID" AND "a"."ID" = 1""".stripMargin)
                 val resultSet = stmt.executeQuery
                 while (resultSet.next()) {
-                    println(s"email: ${resultSet.getString("email")}")
+                    println(
+                        s"""Email: ${resultSet.getString("EMAIL")}
+                        |First name: ${resultSet.getString("FIRST_NAME")}
+                        |Last name: ${resultSet.getString("LAST_NAME")}""".stripMargin)
                 }
             }
         }
